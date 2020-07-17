59d6137c3a2711eefa32767cf99763e899e31201
Felipe Salles <prof.felipe.salles@gmail.com>
2020-07-16 23:30:48 -0300

    first commit

---
+++ src/main/java/com/felipesalles/starwarsplanetsapi/StarwarsplanetsapiApplication.java
@@ -0,0 +1,13 @@
+package com.felipesalles.starwarsplanetsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarwarsplanetsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarwarsplanetsapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipesalles/starwarsplanetsapi/StarwarsplanetsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipesalles.starwarsplanetsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarwarsplanetsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
